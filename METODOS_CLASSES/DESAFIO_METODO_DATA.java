41ab2862cbbfad9ddf1d39b56d594be5977b07a6
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-07 01:52:53 -0300

    revisão de metodos e classes

--- METODOS_CLASSES/DESAFIO_METODO_DATA.java
+++ METODOS_CLASSES/DESAFIO_METODO_DATA.java
@@ -10,7 +10,8 @@ public class DESAFIO_METODO_DATA {
 		return String.format("%d/%d/%d", dia,mes,ano);
 	}
 	void impdataFormatada () {
-		System.out.printf("%d/%d/%d", dia,mes,ano); //duas maneiras de
-													//usar a estrutura de formatr
+		System.out.printf("%d/%d/%d", dia,mes,ano); //duas maneiras de usar a estrutura de formatr
+		//esse metodo void, so retorna em console.		
+		//para usar em aplicações desktop é mais usual o modelo String
 	}
 }
--- METODOS_CLASSES/DESAFIO_METODO_DATA_TESTE.java
+++ METODOS_CLASSES/DESAFIO_METODO_DATA_TESTE.java
@@ -4,6 +4,8 @@ public class DESAFIO_METODO_DATA_TESTE {
 	public static void main(String[] args) {
 	
 		DESAFIO_METODO_DATA d1 = new DESAFIO_METODO_DATA();
+		//classes e variaveis objeto
+		
 		d1.dia = 19;
 		d1.mes = 12;
 		d1.ano = 1980;
@@ -12,6 +14,21 @@ public class DESAFIO_METODO_DATA_TESTE {
 		d2.dia =11;
 		d2.mes = 10;
 		d2.ano = 2000;
+		
+		//metodos de controle e retorno
+		
+		/*
+		var d3 = new DESAFIO_METODO_DATA();
+		d3.dia = 11;
+		d3.mes =11;
+		d3.ano = 2000;
+		
+		
+		
+		System.out.println(d3.dataFormatada());
+		d3.impdataFormatada();
+		*/
+		
 		
 		System.out.println(d1.dataFormatada());
 		System.out.println(d2.dataFormatada());
