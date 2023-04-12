a75cb9e32039563182fb8de3f49555ddfdd2d84d
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-12 01:32:24 -0300

    desafio array

--- ARRAYS/foreach.java
+++ ARRAYS/foreach.java
@@ -15,5 +15,22 @@ public class foreach {
 			System.out.print(nota + " ");
 			
 		}
+		System.out.println();//quebra de linha
+		
+		
+		double [] notas1 = {9.8, 5.7,3.8,10 };
+		
+		double total = 0;
+		
+		for(double nota: notas1) {//outra maneira de usar a função for
+			total += nota;
+		}
+		System.out.println(total);
+		
+		double media = total/4;
+		
+		System.out.println(media);
+
 	}
+	
 }
--- DESAFIO_ARRAYS/DESAFIO01.java
+++ DESAFIO_ARRAYS/DESAFIO01.java
@@ -1,5 +1,29 @@
 package DESAFIO_ARRAYS;
 
-public class DESAFIO01 {
+import java.util.Scanner;
 
+public class DESAFIO01 {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Deseja inserir quantas notas do aluno: ");
+		int qnotas = entrada.nextInt();
+		
+		double [] notas = new double [qnotas];
+		
+		for (int i = 0; i < notas.length; i++) { //loop for dedicado a receber as notas na variavel notas
+			System.out.print("informe a nota: "+ (i+1)+ " : ");
+			notas[i] = entrada.nextInt();
+		}
+		double total = 0;// vai somar as notas inseridas
+		for(double nota : notas){ //loop for somar a quantidade total dada pelas notas
+			total += nota;//total vai somar todas as notas inseridas
+		}
+		double media = total/notas.length; //variavel para calcular a media
+		System.out.println("a media do aluno sera de: " + media + "!");
+		
+		entrada.close();
+	}
 }
