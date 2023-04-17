92623f10ccbdb8eec6d7561db7661a63840d5a81
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-16 23:52:36 -0300

    exercicio

--- ARRAYS/EXERCICIO_FOREACH.java
+++ ARRAYS/EXERCICIO_FOREACH.java
@@ -1,6 +1,34 @@
 package ARRAYS;
 
 public class EXERCICIO_FOREACH {
+	public static void main(String[] args) {
+		//esse script corresponde ao exercicio feito usando um loop for convencional
+		//o intuito desse é por o mesmo exercicio usando foreach
+		
+		double [] notasAlunon = new double [3];
 	
+		notasAlunon [0] = 8.0;
+		notasAlunon [1] = 9.7;
+		notasAlunon [2] = 8.6;
+		
+		double totalaln = 0;
+		for ( double nota :notasAlunon) {
+			totalaln += nota;
+		}
+		System.out.println(totalaln); //total das notas
+		
+		double media = totalaln/3;
+		
+		System.out.printf("a media do aluno é de: %.2f" , media);
+		
+		System.out.println(); //quebra de linha
+		
+		if (media < 7){
+			System.out.println("o aluno foi aprovado");
+		}
+		else {
+			System.out.println("o aluno foi reprovado!");
+		}
+	}
 	
 }
