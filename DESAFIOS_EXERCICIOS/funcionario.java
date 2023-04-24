ad698cb5ca615a00b45191d2a739bb0b42a8c8ab
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-24 19:04:56 -0300

    desafio

--- DESAFIOS_EXERCICIOS/funcionario.java
+++ DESAFIOS_EXERCICIOS/funcionario.java
@@ -9,16 +9,32 @@ public class funcionario {
 		LocalDate admissao = LocalDate.of(2020, 04, 15);
 		dfuncionario f1 = new dfuncionario();
 		f1.nome ="humberto";
-		f1.salario = 1500;
+		f1.salario = 7500;
 		f1.admissao = admissao;
 		
 		int tmpService = f1.calcularts();
-		System.out.printf("o tempo de serviço do funcionario" + f1.nome + "é de " + tmpService +" ano(s)");
+		System.out.printf("o tempo de serviço do funcionario " + f1.nome + " é de " + tmpService +" ano(s)");
 		
 		System.out.println();//quebra de linha
 		
 		double csa = f1.sa(1500);
 		System.out.printf("o decimo terceiro salario, com "
 				+ "a bonificação é de R$%.2f ", csa);
+		System.out.println();
+		System.out.println();
+		
+		LocalDate admissao2 = LocalDate.of(2020, 03, 13);
+		dfuncionario f2 = new dfuncionario();
+		f2.nome = "remerson";
+		f2.salario = 7400;
+		f2.admissao  = admissao2;
+		
+		int tmp = f1.calcularts();
+		System.out.printf("o tempo de serviço do funcionario " + f2.nome + " é de " + tmp +" ano(s)");
+		System.out.println(); //linha
+		
+		double csa2 = f2.sa(1500);
+		System.out.printf("o decimo terceiro salario, com "
+				+ "a bonificação é de R$%.2f ", csa2);
 	}
 }
