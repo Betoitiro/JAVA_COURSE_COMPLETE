ed5b5796ff6a2becf093ac6a8b14e79ef2bc7e13
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-05-10 00:09:10 -0300

    modificadores de acesso

--- ORIENTAÇÃO_OBJETO/encapsulamentoCasaA/Paulo.java
+++ ORIENTAÇÃO_OBJETO/encapsulamentoCasaA/Paulo.java
@@ -2,4 +2,19 @@ package ORIENTAÇÃO_OBJETO.encapsulamentoCasaA;
 
 public class Paulo {
 
+	Ana esposa = new Ana();
+	
+	void testeAcesso() { //sera realizado um teste de acesso da pasta
+		//Segredo
+		//focoDentroDeCasa
+		//formaDeFalar
+		//todosSabem
+		
+		//System.out.println(esposa.segredo); não consegue ser acessado
+		System.out.println(esposa.focoDentroDeCasa);//consegue ser acessado
+		System.out.println(esposa.formaDeFalar);//consegue ser acessado
+		System.out.println(esposa.todosSabem);//consegue ser acessado ja que é um atributo public
+		
+	}
+	
 }
--- ORIENTAÇÃO_OBJETO/encapsulamentoCasaB/Julia.java
+++ ORIENTAÇÃO_OBJETO/encapsulamentoCasaB/Julia.java
@@ -1,5 +1,16 @@
 package ORIENTAÇÃO_OBJETO.encapsulamentoCasaB;
 
+import ORIENTAÇÃO_OBJETO.encapsulamentoCasaA.Ana;
+
 public class Julia {
 
+	void testeAcesso() {
+	
+		Ana sogra = new Ana();
+		
+		System.out.println(sogra.todosSabem);//o unico atributo a ser acessado por julia, sera oq todos sabem.
+		//devido ao nivel de encapsulamento, e como julia não herda ana, ela so consiguira acessar oq for public de ana.
+			
+	}
+	
 }
