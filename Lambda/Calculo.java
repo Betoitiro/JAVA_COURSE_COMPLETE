174f55f650ae1c30a9cb43ae49921ff5064a9866
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-05-16 00:05:02 -0300

    anotation

--- Lambda/Calculo.java
+++ Lambda/Calculo.java
@@ -1,9 +1,15 @@
 package Lambda;
 
+@FunctionalInterface //anotation
 public interface Calculo {
 
 	double executar(double a, double b);
 	
 	//String teste(double c, double d);
 	//se eu colocar outra função na hora de realizar os comandos do calculo 2, ira da erro, ja que vai ter duas funções e isso fara com que o sistema do java n funcione
+	
+	
+	//colocando a @FunctionalInterface, representa uma função que faz com que a interface não receba mais metodo/função
+	//ou seja, o sistema so recebera 1 metodo, caso seja colocado mais uma, sistema começara a dar erro e não ira funcionar
+	
 }
