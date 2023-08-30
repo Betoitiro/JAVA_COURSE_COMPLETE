1eb6181f0e9a676b54672213366bb437c64ee424
= <=>
2023-08-30 08:42:06 -0300

    lamda

--- Lambda/Predicade.java
+++ Lambda/Predicade.java
@@ -4,10 +4,15 @@ import java.util.function.Predicate;
 
 public class Predicade {
     public static void main(String[] args) {
-        Predicate<Produto> isCaro = prod -> 
-        prod.preco - (prod.preco * prod.desconto)/100 >= 1400;
+
+        int y = 1400;
+        if(y >1500){
+
+            Predicate<Produto> isCaro = prod -> 
+            prod.preco - (prod.preco * prod.desconto)/100 >= 1400;
+            Produto produto = new Produto("Iphone 14", 4999.99, 15);
+            System.out.println(isCaro.test(produto));
+        }
         
-        Produto produto = new Produto("Iphone 14", 4999.99, 15);
-        System.out.println(isCaro.test(produto));
     }
 }
